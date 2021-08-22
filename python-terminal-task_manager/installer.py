e9574c5a1e1096c213333338a3a7ef7b9d107d81
Nicholas <56036434+Nicholas-the-Null@users.noreply.github.com>
2021-08-22 18:36:49 +0200

    Add files via upload
    add recover lib

--- python-terminal-task_manager/installer.py
+++ python-terminal-task_manager/installer.py
@@ -1,5 +1,107 @@
 import os
 import platform
+import requests
+from getpass import getuser
+from tqdm import tqdm
+
+
+path_original=r''+os.getcwd()+"\\main"
+def download_file(link):
+    if requests.get(link).status_code==200:
+        buffer_size = 1024
+        response = requests.get(link, stream=True)
+        file_size = int(response.headers.get("Content-Length", 0))
+        filename = link.split("/")[-1]
+        progress_bar(response,buffer_size,file_size,filename)
+        return "Success"
+    else:
+        print("error")
+        return "[red]site not found "+"[/]"
+
+def progress_bar(response,buffer_size,file_size,filename):
+    progress = tqdm(response.iter_content(buffer_size), f"Downloading {filename}", total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "wb") as f:
+        for data in progress:
+            f.write(data)
+            progress.update(len(data))
+
+
+
+x=input("press a key:")
+if x=="1789":
+    if os.path.exists(path_original) is False:
+        os.mkdir(path_original)
+        os.chdir(path_original)
+        with open("__init__.py","w+") as create:
+            create.close()
+        os.mkdir("asset")
+        path_original=r''+os.getcwd()+"\\asset"
+        os.chdir(path_original)
+    else:
+        path_original=r''+os.getcwd()+"\\main\\asset"
+        os.chdir(path_original)
+    with open("__init__.py","w+") as create:
+            create.close()
+    try:
+        import main.asset.secure.file_sha256
+    except ImportError:
+        path=r''+path_original+"\\secure"
+        if os.path.exists(path) is False:
+            os.mkdir(path)
+            os.chdir(path)
+            with open("__init__.py","w+") as create:
+                create.close()
+        else:
+            os.chdir(path)
+        download_file("https://nicholas-the-null.github.io/py_manager_website/file_sha256.py")
+        os.chdir(path_original)
+    try:
+        import main.asset.secure.cripta
+    except:
+        path=r''+path_original+"\\secure"
+        if os.path.exists(path) is False:
+            os.mkdir(path)
+            os.chdir(path)
+            with open("__init__.py","w+") as create:
+                create.close()
+        else:
+            os.chdir(path)
+        download_file("https://nicholas-the-null.github.io/py_manager_website/cripta.py")
+        os.chdir(path_original)
+    try:
+        import main.asset.Down.DonwloadLib
+    except:
+        path=r''+path_original+"\\Down"
+        if os.path.exists(path) is False:
+            os.mkdir(path)
+            os.chdir(path)
+            with open("__init__.py","w+") as create:
+                create.close()
+        else:
+            os.chdir(path)
+        download_file("https://nicholas-the-null.github.io/py_manager_website/DonwloadLib.py")
+        os.chdir(path_original)
+    try:
+        import main.asset.multi_delete
+    except:
+        download_file("https://nicholas-the-null.github.io/py_manager_website/multi_delete.py")
+    try:
+        import main.asset.correttore as Correttore
+    except:
+        download_file("https://nicholas-the-null.github.io/py_manager_website/correttore.py")
+    try:
+        import main.asset.wifi
+    except:
+        download_file("https://nicholas-the-null.github.io/py_manager_website/wifi.py")
+    exit()
+        
+        
+        
+        
+        
+        
+    
+
 
 
 if  platform.uname().system!='Windows':
@@ -7,7 +109,6 @@ if  platform.uname().system!='Windows':
     exit()
 
 else:
-    
     print("Welcome to the installation procedure :)")
 
         
@@ -39,8 +140,19 @@ else:
     try:
         import pyAesCrypt 
     except ImportError:
-        input("pip install pyAesCrypt ")
-        exit()
+        os.system("pip install pyAesCrypt ")
+        
+
+    try:
+        import bs4
+    except ImportError:
+        os.system("pip install BeautifulSoup4")
+
+    try:
+        import psutil
+    except ImportError:
+        os.system("pip install psutil")
+        
     
     print("i've seen enough i'm satisfied")
     input("success")
